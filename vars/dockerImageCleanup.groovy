64d0d8a7bbd45eee041b4ea83e06318f850341c5
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-06-02 12:18:05 +0530

    Update dockerImageCleanup.groovy

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,7 +1,7 @@
 def call(String project, String ImageTag, String hubUser){
     
     sh """
-     docker rmi ${hubUser}/${project}:${ImageTag}
+     //docker rmi ${hubUser}/${project}:${ImageTag}
      docker rmi ${hubUser}/${project}:latest
     """
 }
